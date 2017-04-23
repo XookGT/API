5c7ca07925fa8f04a7dc657e57f2f99b87f105d8
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-04-22 23:12:31 -0600

    add AuthController

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class ApiAuthController extends Controller
 {
-    //
+    public function userAuth(Request $request)
+    {
+        $credentials = $request->only('email','password','role');
+        $token = null;
+
+        try{
+            if(!$token = JWTAuth::attemp($credentials))
+            {
+                return response()->json(['Erro' => 'Invalid Credentials'],400);
+            }
+
+        }catch(JWTException $ex)
+        {
+            return response()->json(['Error' => $ex->getMessage()],500);
+        }
+    }
 }
