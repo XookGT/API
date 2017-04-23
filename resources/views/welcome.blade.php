9a81f332e3ca1a49a46dd7c847b5a7eecf91702c
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-04-22 19:35:22 -0600

    add menus to the view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,10 +84,9 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://facebook.com">FACEBOOK</a>
+                    <a href="https://twitter.com/">TWITTER</a>
+                    <a href="http://www.simbitgt.com">SIMBit</a>
                 </div>
             </div>
         </div>
