285e55575871f0fc9485fd9951976153578f0492
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-04-23 11:35:03 -0600

    prueba

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -11,7 +11,7 @@ class ApiAuthController extends Controller
     public function authenticate(Request $request)
     {
         // grab credentials from the request
-        $credentials = $request->only('email', 'password');
+        $credentials = $request->only('email', 'password', 'name');
 
         try {
             // attempt to verify the credentials and create a token for the user
