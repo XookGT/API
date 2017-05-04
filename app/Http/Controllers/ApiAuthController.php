900e6d0dd7983e8f9cc7062ab33be46ffbd28391
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-03 22:49:06 -0600

    add

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -11,20 +11,28 @@ class ApiAuthController extends Controller
     public function authenticate(Request $request)
     {
         // grab credentials from the request
+
+           header("Access-Control-Allow-Origin: *");
+
+            // ALLOW OPTIONS METHOD
+            $headers = [
+                    'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+                    'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+                ];
         $credentials = $request->only('email', 'password', 'name');
 
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 401);
+                return response()->json(['error' => 'invalid_credentials'], 401,$headers);
             }
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
-            return response()->json(['error' => 'could_not_create_token'], 500);
+            return response()->json(['error' => 'could_not_create_token'], 500,$headers);
         }
 
         // all good so return the token
-        return response()->json(compact('token'));
+        return response()->json(compact('token'),$headers);
     }
 }
 
