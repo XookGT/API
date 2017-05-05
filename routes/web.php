e2e71e2581daa2d72d44de242fc6a8eee378da8a
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-04 23:45:08 -0600

    add web routes and api routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ Route::get('/', function () {
 });
 
 Route::post('auth_login', 'ApiAuthController@authenticate');
+
+Route::resource('/bank','Banks');
+Route::resource('/bill','Bills');
+Route::resource('/bill-state','BillStates');
+Route::resource('/categorie','Categories');
+Route::resource('/city','Cities');
+Route::resource('/country','Countries');
+Route::resource('/course','Courses');
+Route::resource('/day','Days');
+Route::resource('/level','Levels');
+Route::resource('/payment-method','PaymentMethods');
+Route::resource('/payment','Payments');
+Route::resource('/payment-state','PaymentStates');
+Route::resource('/place','Places');
+Route::resource('/profile-status','ProfileStatuses');
+Route::resource('/province','Provinces');
+Route::resource('/purchase-order','PurchaseOrders');
+Route::resource('/rol','Roles');
+Route::resource('/score','Scores');
+Route::resource('/state-purchase-order','StatePurchaseOrders');
+Route::resource('/state-tutotial-payment','StateTutorialPayments');
+Route::resource('/tutorial-day','TutorialDays');
+Route::resource('/tutorial-has-place','TutorialHasPlaces');
+Route::resource('/tutorial','Tutorials');
+Route::resource('/tutorial-payment','TutorialsPayments');
+Route::resource('/tutor-payment','TutorPayments');
+Route::resource('/xookcc','XookCCs');
