a9cfa55436058dd2d470d84d3aac2b5aa3388c80
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-04 23:50:31 -0600

    fix route

--- app/Http/Controllers/PurchaseOrders.php
+++ app/Http/Controllers/PurchaseOrders.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PurcharseOrders extends Controller
+class PurchaseOrders extends Controller
 {
     /**
      * Display a listing of the resource.
