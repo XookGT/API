ef7ee23b28d98afdbecec342d530faf612c8a074
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-06 17:17:54 -0600

    add code on model Level

--- app/Level.php
+++ app/Level.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Level extends Model
 {
     //Niveles de Cursos, cada nivel pertenece a una categoria
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+            'name',
+            'starts',
+            'rank',
+    ];
 }
