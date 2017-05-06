e93b2a9b404f033128b91226324d389a76d8877f
Dayton Garcia <dayton.garcia@simbitgt.com>
2017-05-06 00:22:34 -0600

    fix

--- routes/api.php
+++ routes/api.php
@@ -19,37 +19,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post('auth_login', 'ApiAuthController@authenticate');
-Route::group(['middleware' => 'cors'], function() {
-    //
-    
-    Route::resource('/bank','Banks');
-    Route::resource('/bill','Bills');
-    Route::resource('/bill-state','BillStates');
-    Route::resource('/categorie','Categories');
-    Route::resource('/city','Cities');
-    Route::resource('/country','Countries');
-    Route::resource('/course','Courses');
-    Route::resource('/day','Days');
-    Route::resource('/level','Levels');
-    Route::resource('/payment-method','PaymentMethods');
-    Route::resource('/payment','Payments');
-    Route::resource('/payment-state','PaymentState');
-    Route::resource('/place','Places');
-    Route::resource('/profile-status','ProfileStatuses');
-    Route::resource('/province','Provinces');
-    Route::resource('/purchase-order','PurchaseOrders');
-    Route::resource('/rol','Roles');
-    Route::resource('/score','Scores');
-    Route::resource('/state-purchase-order','StatePurchaseOrders');
-    Route::resource('/state-tutotial-payment','StateTutorialPayments');
-    Route::resource('/tutorial-day','TutorialDays');
-    Route::resource('/tutorial-has-place','TutorialHasPlaces');
-    Route::resource('/tutorial','Tutorials');
-    Route::resource('/tutorial-payment','TutorialPayments');
-    Route::resource('/tutor-payment','TutorPayments');
-    Route::resource('/xookcc','XookCCs');
-    Route::get('/categorie-name/{name}', 'Categories@SearchByName');
-
-});
+Route::resource('/bank','Banks');
+Route::resource('/bill','Bills');
+Route::resource('/bill-state','BillStates');
+Route::resource('/categorie','Categories');
+Route::resource('/city','Cities');
+Route::resource('/country','Countries');
+Route::resource('/course','Courses');
+Route::resource('/day','Days');
+Route::resource('/level','Levels');
+Route::resource('/payment-method','PaymentMethods');
+Route::resource('/payment','Payments');
+Route::resource('/payment-state','PaymentState');
+Route::resource('/place','Places');
+Route::resource('/profile-status','ProfileStatuses');
+Route::resource('/province','Provinces');
+Route::resource('/purchase-order','PurchaseOrders');
+Route::resource('/rol','Roles');
+Route::resource('/score','Scores');
+Route::resource('/state-purchase-order','StatePurchaseOrders');
+Route::resource('/state-tutotial-payment','StateTutorialPayments');
+Route::resource('/tutorial-day','TutorialDays');
+Route::resource('/tutorial-has-place','TutorialHasPlaces');
+Route::resource('/tutorial','Tutorials');
+Route::resource('/tutorial-payment','TutorialPayments');
+Route::resource('/tutor-payment','TutorPayments');
+Route::resource('/xookcc','XookCCs');
+Route::get('/categorie-name/{name}', 'Categories@SearchByName');
 
 
